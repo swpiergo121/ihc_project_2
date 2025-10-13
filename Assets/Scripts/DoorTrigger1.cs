c5c1ed9b6606c918da6f6749c6dab1b614056bcc
swpiergo121 <pierojgj@hotmail.com>
2025-10-13 18:38:07 -0500

    Add template for meta project

---
+++ Assets/Scripts/DoorTrigger1.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoorTrigger1 : MonoBehaviour
+{
+    public GameObject doorHinge; // Now referencing the HINGE object, not the door mesh!
+    private bool isOpen = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (!isOpen)
+        {
+            doorHinge.transform.Rotate(0, 75, 0); // Rotating around hinge point
+            isOpen = true;
+        }
+    }
+
+    private void CloseDoor()
+    {
+        if (isOpen)
+        {
+            doorHinge.transform.Rotate(0, -75, 0);
+            isOpen = false;
+        }
+    }
+}
