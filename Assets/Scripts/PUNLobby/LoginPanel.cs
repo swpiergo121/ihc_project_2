8086d72e2028fe661ed75be1f8bd53de1e78b71e
giansegg <gianpier.segovia@utec.edu.pe>
2025-11-18 01:45:11 -0500

    menuss w scripts

--- Assets/Scripts/PUNLobby/LoginPanel.cs
+++ Assets/Scripts/PUNLobby/LoginPanel.cs
@@ -7,6 +7,8 @@ namespace PUNLobby
     public class LoginPanel : MonoBehaviour
     {
         [SerializeField] private InputField nameInputField;
+        //[SerializeField] private GameObject panelMenuPrincipal;
+
         private const string LAST_LOGIN = "/last_login.txt";
 
         private void OnEnable()
@@ -27,6 +29,20 @@ namespace PUNLobby
             launcher.Connect(playerName);
             SerializeUtility.SaveContent(Application.persistentDataPath + LAST_LOGIN, playerName);
             launcher.PanelManager.infoPanel.Show(400, 200, "Connecting...");
+
+            // Oculta este panel (el LoginPanel)
+            /*this.gameObject.SetActive(false);
+
+            // Muestra el Panel_MenuPrincipal
+            if (panelMenuPrincipal != null)
+            {
+                panelMenuPrincipal.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Referencia a 'panelMenuPrincipal' no asignada en el Inspector.");
+            }
+            */
         }
 
         public void ExitGame()
